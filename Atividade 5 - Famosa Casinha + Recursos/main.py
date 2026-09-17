c7344b340c5f7814bb761170e2490fb5cbb46007
Diego <diegogcb@outlook.com>
2026-09-17 16:32:47 -0300

    criando a casa

--- Atividade 5 - Famosa Casinha + Recursos/main.py
+++ Atividade 5 - Famosa Casinha + Recursos/main.py
@@ -12,7 +12,18 @@ while running:
             running = False
 
     # Fundo
+    #draw.rect(screen, color, (x, y, largura, altura))
     screen.fill("#97D1FA")
+
+    #grama
+    draw.rect(screen, "green", (0, 600, 1280, 120))
+
+    # Casa
+    draw.rect(screen, "#646464", (310, 360, 300, 240))
+
+    # Telhado
+    draw.polygon(screen, "#FF0000", [(310, 360), (610, 360), (460, 250)])
+
 
     display.update()
 
